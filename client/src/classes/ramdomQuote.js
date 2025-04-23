82710cf74d2b003762f42370b0a3e26c87883ef2
AndrewSide <agorban2396@gmail.com>
2025-04-23 21:18:27 +0300

    Conditional import of the condig in the client app

--- client/src/classes/ramdomQuote.js
+++ client/src/classes/ramdomQuote.js
@@ -1,7 +1,15 @@
 import { quotes, jokes } from "../data/quotes.js";
 import MathUtils from "../utils/mathUtils.js";
 import Quote from "./quote.js";
-import config from "../../config.js";
+// import config from "../../config.js";
+
+let config 
+
+try {
+  config = await import("../../config.prod.js")
+} catch (error) {
+  config = await import("../../config.js")
+}
 
 class RandomQuote {
   static getRandomQuote() {
